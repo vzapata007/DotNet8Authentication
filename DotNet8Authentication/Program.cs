e3f267bf867c349625e14898a8c0bafaa666cb20
Victor Zapata <victor.zapata@msn.com>
2024-07-05 22:35:02 +1200

    Se agregan comentarios a clase Program

--- DotNet8Authentication/Program.cs
+++ DotNet8Authentication/Program.cs
@@ -1,13 +1,14 @@
+// Importing necessary namespaces
 using DotNet8Authentication.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.Filters;
 
+// Creating a new web application builder
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Adding services to the container
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -24,22 +25,26 @@ builder.Services.AddSwaggerGen(options =>
     options.OperationFilter<SecurityRequirementsOperationFilter>();
 });
 
+// Adding a DbContext to the services container
 builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Adding authorization services
 builder.Services.AddAuthorization();
 
+// Adding Identity API endpoints with Entity Framework stores
 builder.Services.AddIdentityApiEndpoints<IdentityUser>().AddEntityFrameworkStores<DataContext>();
 
-
+// Building the app
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configuring the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+// Configuring a mapping for the Identity API in the app object to work with the IdentityUser class
 app.MapIdentityApi<IdentityUser>();
 
 app.UseHttpsRedirection();
